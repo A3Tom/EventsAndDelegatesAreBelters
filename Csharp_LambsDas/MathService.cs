9581a89fa23a8543aa62c4703f72948ffa6271be
A3Tom <Tom_Esq@Yahoo.com>
2020-02-01 15:55:05 +0000

    I have created a calculation vessel. I have crafted an IOC mathematical Noah's ark

--- Csharp_LambsDas/MathService.cs
+++ Csharp_LambsDas/MathService.cs
@@ -8,14 +8,15 @@ namespace Csharp_LambsDas
         //public event EventHandler<MathPerformedEventArgs> MathPerformed;
 
         public delegate void MathPerformedHandler(double result);
+        public delegate double CalculationHandler(double value1, double value2);
 
         public event MathPerformedHandler MathPerformed;
 
-        public void MultiplyNumbers(double value1, double value2)
+        public void CalculateNumbers(double value1, double value2, CalculationHandler calculation)
         {
             Timer timer = new Timer(5000);
 
-            MathPerformed(value1 * value2);
+            MathPerformed(calculation(value1, value2));
         }
 
     }
